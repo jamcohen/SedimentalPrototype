882c1a41a8cc8c4a2ff132668a1c113a00aaa622
Jacob Cohen <Jake@dhcp-197-76.cruznet.ucsc.edu>
2014-11-19 16:07:16 -0800

    Got physics and slicing to work

--- scripts/storm/main.js
+++ scripts/storm/main.js
@@ -20,6 +20,11 @@ window.onload = function () {
         var poly = new Sliceable(400,100, points);
         poly.active = true;
         
+        poly.sprite.body.destroy();
+        //poly.sprite.rotation = Math.PI/3;
+        game.physics.p2.enable(poly.sprite, Phaser.Physics.P2JS, true);
+        poly.refresh();
+        
         //poly.slice(0,0,800,600);
         cursors = game.input.keyboard.createCursorKeys();
         
@@ -37,10 +42,13 @@ window.onload = function () {
           for(var i=0; i<sliceables.length; i++){
               
               if(cursors.right.isDown){
-                   sliceables[i].sprite.x += 1;
+                   //sliceables[i].sprite.x += 1;
+                  sliceables[i].sprite.rotation += 0.05;
+                  sliceables[i].sprite.body.rotateLeft(20);
               }
               if(cursors.left.isDown){
-                   sliceables[i].sprite.x -= 1;
+                  //sliceables[i].sprite.x -= 1;
+                  sliceables[i].sprite.rotation -= 0.05;
               }
               if(cursors.up.isDown){
                    sliceables[i].sprite.y -= 1;
@@ -64,17 +72,16 @@ function slice(){
 }
 
 //xPos and yPos are in global space. points is in local.
-function Sliceable(xPos, yPos, points){
+function Sliceable(xPos, yPos, points, velocity){    
     this.sprite = game.add.sprite(xPos, yPos);
     this.shape = game.add.graphics();
     this.sprite.addChild(this.shape);
-    //game.physics.p2.enable(this.sprite);
     game.physics.p2.enable(this.sprite, Phaser.Physics.P2JS, true);
     
     this.points = points;
     this.active = false;
     
-    this.refresh();
+    this.refresh(velocity);
     sliceables.push(this);
 }
 
@@ -87,44 +94,45 @@ Sliceable.prototype.slice = function(x1, y1, x2, y2){
     y2 -= this.sprite.y;
     
     var angle = this.sprite.rotation;
-    point1 = rotateAround(0, 0, angle, x1, y1);
-    point2 = rotateAround(0, 0, angle, x2, y2);
+    point1 = rotateAround(0, 0, angle, x1, y1, false);
+    point2 = rotateAround(0, 0, angle, x2, y2, false);
     
     console.log(point1, point2);
     
-    var ruler = game.add.graphics(0,0);
-    ruler.lineStyle(3, 0x00FF00, 1);
-    ruler.moveTo(point1.x+this.sprite.x,point1.y+this.sprite.y);
-    ruler.lineTo(point2.x+this.sprite.x,point2.y+this.sprite.y);
-    this.sprite.addChild(ruler);
+    //this.sprite.addChild(ruler);
     var splitPolys = PolyK.Slice(this.points, point1.x, point1.y, point2.x, point2.y);
     
     //no new polys were formed.
     if(splitPolys.length <= 1){
         return;
     }
-    
+    var vel = this.sprite.body.data.velocity;
+    var worldPosX = this.sprite.x;
+    var worldPosY = this.sprite.y;
     for(var i = 0; i<splitPolys.length; ++i){
+        //center = {x:0, y:0};
         if(PolyK.ContainsPoint(splitPolys[i], 0, 0)){
-            console.log("contained point");
+            rotatePoints(splitPolys[i], 0, 0, angle, true);
+            var centroid = getCentroid(splitPolys[i]);
+            moveOrigin(splitPolys[i], centroid.x, centroid.y);
             this.points = splitPolys[i];
-            this.refresh();
+            this.sprite.x += centroid.x;
+            this.sprite.y += centroid.y;
+            
+            //force rotation to 0
+            this.sprite.body.destroy();
+            this.sprite.rotation = 0;
+            game.physics.p2.enable(this.sprite, Phaser.Physics.P2JS, true);
+            
+            this.refresh(vel);
             continue;
         }else{
-            var centerX = 0;
-            var centerY = 0;
-            for(var j = 0; j<splitPolys[i].length; j+=2){
-                centerX += splitPolys[i][j];
-                centerY += splitPolys[i][j+1];
-            }
-            centerX /= Math.floor(splitPolys[i].length/2);
-            centerY /= Math.floor(splitPolys[i].length/2);
-            for(var j = 0; j<splitPolys[i].length; j+=2){
-                splitPolys[i][j] -= centerX;
-                splitPolys[i][j+1] -= centerY;
-            }
-            var newChunk = new Sliceable(centerX+this.sprite.x, centerY+this.sprite.y, splitPolys[i]);
-            
+            rotatePoints(splitPolys[i], 0, 0, angle, true);
+            var centroid = getCentroid(splitPolys[i]);
+            moveOrigin(splitPolys[i], centroid.x, centroid.y);
+            //var newChunk = new Sliceable(worldPosX, worldPosY, splitPolys[i]);
+            console.log(vel);
+            var newChunk = new Sliceable(centroid.x+worldPosX, centroid.y+worldPosY, splitPolys[i]);
         }
     }
     //sliceables[0].shape.clear();
@@ -134,10 +142,16 @@ Sliceable.prototype.slice = function(x1, y1, x2, y2){
     console.log(sliceables.length, splitPolys);
 }
 
-Sliceable.prototype.refresh = function(){
-    this.sprite.body.clearShapes();
-    this.sprite.body.addPolygon({}, this.points);
+//Draws and initializes the physics shapes
+//velocity is an optional argument. Used to allow new slices to preserve their previous velocity
+Sliceable.prototype.refresh = function(velocity){
+    //velocity = (typeof velocity !== 'undefined') ? velocity : {x:0, y:0}; //sets 0,0 as default value for velocity
     
+    this.sprite.body.clearShapes();
+    this.sprite.body.addPolygon({position:[0,0]}, this.points);
+    if(typeof velocity !== 'undefined'){
+        this.sprite.body.data.velocity = velocity;
+    }
     this.draw();
 }
 
@@ -168,13 +182,98 @@ function initPhysics(){
       game.physics.p2.gravity.y = 100;
 }
 
-function rotateAround(centerX, centerY, angle, x, y){
-    var newX = centerX + (x-centerX)*Math.cos(angle) - (y-centerY)*Math.sin(angle);
-    var newY = centerY + (x-centerX)*Math.sin(angle) + (y-centerY)*Math.cos(angle);
+//finds the average of a group of points
+//points is a flat array of type [x,y,x,y,x,y,x,y]
+function getCenter(points){
+    var centerX = 0;
+    var centerY = 0;
     
-    console.log(angle);
-    console.log(centerX);
-    console.log(centerY);
-    
+    for(var i = 0; i<points.length; i+=2){
+        centerX += points[i];
+        centerY += points[i+1];
+    }
+    centerX /= Math.floor(points.length/2);
+    centerY /= Math.floor(points.length/2);
+    return {x:centerX, y:centerY}
+}
+
+//Gets the center of mass of a polygon
+function getCentroid(points)
+{
+    var vertices = getPointsFromFlatArray(points)
+    var centroid = {x:0, y:0};
+    var signedArea = 0.0;
+    var x0 = 0.0; // Current vertex X
+    var y0 = 0.0; // Current vertex Y
+    var x1 = 0.0; // Next vertex X
+    var y1 = 0.0; // Next vertex Y
+    var a = 0.0;  // Partial signed area
+
+    // For all vertices except last
+    var i=0;
+    for (i=0; i<vertices.length-1; ++i)
+    {
+        x0 = vertices[i].x;
+        y0 = vertices[i].y;
+        x1 = vertices[i+1].x;
+        y1 = vertices[i+1].y;
+        a = x0*y1 - x1*y0;
+        signedArea += a;
+        centroid.x += (x0 + x1)*a;
+        centroid.y += (y0 + y1)*a;
+    }
+
+    // Do last vertex
+    x0 = vertices[i].x;
+    y0 = vertices[i].y;
+    x1 = vertices[0].x;
+    y1 = vertices[0].y;
+    a = x0*y1 - x1*y0;
+    signedArea += a;
+    centroid.x += (x0 + x1)*a;
+    centroid.y += (y0 + y1)*a;
+
+    signedArea *= 0.5;
+    centroid.x /= (6.0*signedArea);
+    centroid.y /= (6.0*signedArea);
+
+    return centroid;
+}
+
+//adjust a group of points to be relative to a new origin
+//points is a flat array of type [x,y,x,y,x,y,x,y]
+function moveOrigin(points, newOriginX, newOriginY){
+    for(var i = 0; i<points.length; i+=2){
+        points[i] -= newOriginX;
+        points[i+1] -= newOriginY;
+    }
+}
+
+function rotateAround(centerX, centerY, angle, x, y, clockwise){
+    var newY, newX;
+    if(!clockwise){
+        newX = centerX + (x-centerX)*Math.cos(angle) + (y-centerY)*Math.sin(angle);
+        newY = centerY - (x-centerX)*Math.sin(angle) + (y-centerY)*Math.cos(angle);
+    }else{
+        newX = centerX + (x-centerX)*Math.cos(angle) - (y-centerY)*Math.sin(angle);
+        newY = centerY + (x-centerX)*Math.sin(angle) + (y-centerY)*Math.cos(angle);
+    }
+
     return {x: newX, y:newY};
+}
+
+function rotatePoints(points, centerX, centerY, angle, clockwise){
+    for(var i = 0; i<points.length; i+=2){
+        var newPoints = rotateAround(centerX, centerY, angle, points[i], points[i+1], clockwise);
+        points[i] = newPoints.x;
+        points[i+1] = newPoints.y;
+    }
+}
+
+function getPointsFromFlatArray(points){
+    result = [];
+    for(var i=0;i<points.length;i+=2){
+        result.push({x:points[i], y:points[i+1]});
+    }
+    return result;
 }
